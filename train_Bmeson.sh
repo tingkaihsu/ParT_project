aebd1b19016ee01b590146dbd857e136126a4e69
TK <tingkaihsu0924@gmail.com>
2024-08-01 23:27:04 +0800

    refresh something

--- train_Bmeson.sh
+++ train_Bmeson.sh
@@ -69,7 +69,7 @@ weaver \
     --data-config data/Bmeson/bm_${FEATURE_TYPE}.yaml --network-config $modelopts \
     --model-prefix training/Bmeson/${model}/{auto}${suffix}/net \
     --num-workers 1 --fetch-step 1 --in-memory --train-val-split 0.60 \
-    --batch-size 128 --samples-per-epoch 160000 --samples-per-epoch-val 20000 --num-epochs 1 --gpus 0 \
+    --batch-size 128 --samples-per-epoch 160000 --samples-per-epoch-val 20000 --num-epochs 20 --gpus 0 \
     --start-lr $lr --optimizer ranger --log logs/Bmeson_${model}_{auto}${suffix}.log --predict-output pred.root \
     --tensorboard Bmeson_${FEATURE_TYPE}_${model}${suffix} \
     --regression-mode \
