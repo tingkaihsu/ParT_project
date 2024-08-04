87f8ebc791629307f64f719e491738628274b40e
TK <tingkaihsu0924@gmail.com>
2024-08-04 20:35:10 +0800

    add reading parquet file

---
+++ utils/read_parquet.py
@@ -0,0 +1,11 @@
+import pandas as pd
+def readFile(filepath, filepath1):
+    df = pd.read_parquet(filepath)
+    df1 = pd.read_parquet(filepath1)
+    print(df1['part_charge'])
+    # df1['part_px'][0] = df['part_px'][0]
+    # print(df1['part_px'][0])
+    del df
+    del df1
+# readFile('out.csv')
+readFile('../data/Bmeson/test_file.parquet','../data/Bmeson/train_file.parquet')
